f03543b243046990e0e9701742ecf8d27b9ea955
ori halamit <orihalumi1@gmail.com>
2018-05-29 11:22:00 +0300

    GameHud and ShopScreen update

--- Project/HudScreen.cpp
+++ Project/HudScreen.cpp
@@ -2,8 +2,20 @@
 
 
 
-HudScreen::HudScreen() 
-	: IScreen(false) {}
+HudScreen::HudScreen(const sf::Vector2f& size, std::shared_ptr<Player> p)
+	: IScreen(false), m_rect(sf::Vector2f(size.x,size.y/9)), m_p(p) {
+	int widthJumps = (int)size.x / 4;
+	m_rect.setFillColor(sf::Color::Black);
+	m_rect.setOutlineColor(sf::Color::White);
+	m_rect.setOutlineThickness(2);
+	m_rect.setPosition(0, 0);
+	auto font = Resources::getInstance().getFontsMap()->getResource(MENU_FONT);
+	m_em = std::make_shared<EventsManager>();
+	m_menu = std::make_shared<MenuManager>(m_em);
+	m_menu->addButtom(std::make_shared<Button>(m_em, sf::Vector2f(widthJumps * 1, 20), sf::Vector2f(120, 50), sf::Color::White, sf::Color::Red, *font, "HP:"));
+	m_menu->addButtom(std::make_shared<Button>(m_em, sf::Vector2f(widthJumps * 2, 20), sf::Vector2f(120, 50), sf::Color::White, sf::Color::Red, *font, "Ammo:"));
+	m_menu->addButtom(std::make_shared<Button>(m_em, sf::Vector2f(widthJumps * 3, 20), sf::Vector2f(120, 50), sf::Color::White, sf::Color::Red, *font, "FPS:"));
+}
 
 
 HudScreen::~HudScreen()
@@ -31,4 +43,6 @@ sf::FloatRect HudScreen::getRect() const
 
 void HudScreen::drawScreen(sf::RenderWindow & window)
 {
+	window.draw(m_rect);
+	m_menu->drawMenu(window);
 }
